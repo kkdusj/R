a19ee651fbbffada93cfbeb6dbf5d92719a24c69
kkdusj <104974463+kkdusj@users.noreply.github.com>
2022-09-13 05:22:32 +0200

    Add files via upload

--- black.lua
+++ black.lua
@@ -17159,7 +17159,7 @@ data = {
 {text = '❨ 𝑫𝒆𝑽 𝑺𝒐𝑼𝒓𝑪𝒆 ❩', url = 't.me/TR_E2S_ON_MY_MOoN'}, 
 },
 { 
-{text = 'مـطـور الـبـوت♻️', url = "https://t.me/"..bb.username..""},
+{text = 'مـطـور الـبـوت♻️', url = 't.me/'..UserBot..'?startgroup=new'}, 
 },
 {
 {text = '➕ اضفني لمجموعتك', url = 't.me/'..UserBot..'?startgroup=new'}, 
